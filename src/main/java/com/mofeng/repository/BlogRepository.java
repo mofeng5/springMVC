88404e537edbaafb85e285955289e93ef88e618d
= <=>
2017-03-30 17:11:19 +0800

    增加博客文章管理功能

--- src/main/java/com/mofeng/repository/BlogRepository.java
+++ src/main/java/com/mofeng/repository/BlogRepository.java
@@ -2,11 +2,24 @@ package com.mofeng.repository;
 
 import com.mofeng.model.BlogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * Created by mofeng on 2017/3/30.
  */
 @Repository
 public interface BlogRepository extends JpaRepository<BlogEntity, Integer>{
+
+    //修改博文操作
+    @Modifying
+    @Transactional
+    @Query("update BlogEntity blog set blog.title=:qTitle, blog.userByUserId.id=:qUserId, blog.content=:qContent, blog.pubDate=:qPubDate where blog.id=:qId")
+    public void updateBlog(@Param("qTitle") String title, @Param("qUserId") int userId, @Param("qContent") String content,
+                    @Param("qPubDate") Date pubDate, @Param("qId") int id);
 }
